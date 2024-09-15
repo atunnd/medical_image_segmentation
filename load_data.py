8de5ba974e32148a20ab17544ef7d67194347bf3
atunnd <atuan.ngndai@gmail.com>
2024-09-15 12:31:41 +0700

    Modify the data loader for 2D unet model

--- load_data.py
+++ load_data.py
@@ -8,6 +8,7 @@ import SimpleITK as sitk
 import numpy as np
 import shutil
 
+
 class CustomDataset(tf.keras.utils.Sequence):
 
     '''
@@ -22,11 +23,14 @@ class CustomDataset(tf.keras.utils.Sequence):
             - task: the task is in the list ['La Cavity', 'Panceras CT']
         2. output: none
     '''
-    def __init__(self, training_path, testing_path, input_size = 112, normalization = 'standard',
-                 load_train = True, batch_size = 16, n_train_patients = 10, n_test_patients = 5, task = 'LA Cavity'):
-        
+
+    def __init__(self, training_path, testing_path, input_size=112, normalization='standard',
+                 load_train=True, batch_size=1, n_train_patients=10, n_test_patients=5, task='LA Cavity'):
+
+        super().__init__()
+
         self.training_path = training_path
-        self.testing_path= testing_path
+        self.testing_path = testing_path
         self.input_size = input_size
         self.normalization = normalization
         self.load_train = load_train
@@ -46,14 +50,14 @@ class CustomDataset(tf.keras.utils.Sequence):
                 shutil.rmtree(dir2)
             os.makedirs('LA_subset')
             os.makedirs('LA_subset/log')
-        
+
         self.train_files = os.listdir(self.training_path)
         self.test_files = os.listdir(self.testing_path)
-
 
     '''
     function: the function will be called at the end of every epoch by fit method
     '''
+
     def on_epoch_end(self):
         pass
 
@@ -62,36 +66,45 @@ class CustomDataset(tf.keras.utils.Sequence):
         1. intput: self
         2. output: a batch of data (x, y)
     '''
-    def __getitem__(self):
-        pass
 
-    '''
-    function: get length of the whole data
-        1. input: self
-        2. output: length of the data
-    '''
+    def __getitem__(self, index):
+        x, y = self.get_data()
+        x_tensor = tf.convert_to_tensor(x, dtype=tf.float32)
+        y_tensor = tf.convert_to_tensor(y, dtype=tf.float32)
+        print(f"x: {np.shape(x_tensor)}")
+        print(f"y: {np.shape(y_tensor)}")
+        return x_tensor[index], y_tensor[index]
+
+
     def __len__(self):
-        pass
+        return (self.n_train_patients * np.shape(self.get_input())[0]) // self.batch_size
 
     '''
     helper function: this function load nnrd file into a 3D matrix
         1. input: filenam_path
         2. output with shape = #slices x width x length
     '''
+
     def load_nrrd(self, filename_path):
         data = sitk.ReadImage(filename_path)						# read in image
-        data = sitk.Cast(sitk.RescaleIntensity(data),sitk.sitkUInt8)	# convert to 8 bit (0-255)
+        # convert to 8 bit (0-255)
+        data = sitk.Cast(sitk.RescaleIntensity(data), sitk.sitkUInt8)
         data = sitk.GetArrayFromImage(data)								# convert to numpy array
-        return(data)
-    
-    def get_data(self, batches):
-        pass
+        return (data)
+
+    def get_data(self):
+
+        input_samples = self.get_input()
+        output_samples = self.get_output()
+        
+        return np.array(input_samples), np.stack([np.array(output_samples[:, :, :, 0]), np.array(output_samples[:, :, :, 1])], axis=-1)
 
     '''
     function: a helper function for loading input data
         1. input: self
         2. output: input data
     '''
+
     def get_input(self):
         images = []
 
@@ -100,35 +113,41 @@ class CustomDataset(tf.keras.utils.Sequence):
         else:
             number_of_patient = self.n_test_patients
 
-        for patient in number_of_patient:
+        for patient in range(number_of_patient):
 
             if self.load_train:
-                raw_mri_sample = load_nrrd(os.path.join("../dataset/LA_dataset/Training Set", self.train_files[patient], 'lgemri.nrrd'))
+                raw_mri_sample = self.load_nrrd(os.path.join(
+                    "../dataset/LA_dataset/Training Set", self.train_files[patient], 'lgemri.nrrd'))
             else:
-                raw_mri_sample = load_nrrd(os.path.join("../dataset/LA_dataset/Testing Set", self.test_files[patient], 'lgemri.nrrd'))
-            
+                raw_mri_sample = self.load_nrrd(os.path.join(
+                    "../dataset/LA_dataset/Testing Set", self.test_files[patient], 'lgemri.nrrd'))
+
             # move the dimension of slice into the last axis
-            raw_mri_sample = np.rollaxis(raw_mri_sample, 0, 3) # (width, height, slice)
-        
+            raw_mri_sample = np.rollaxis(
+                raw_mri_sample, 0, 3)  # (width, height, slice)
+
             # based off the image size, and the specified input size, find coordinates to crop image
-	        # the input size of the model is 112
+            # the input size of the model is 112
             midpoint = raw_mri_sample.shape[0]//2
-            n11, n12 = midpoint - int(self.input_size/2), midpoint + int(self.input_size/2)
+            n11, n12 = midpoint - \
+                int(self.input_size/2), midpoint + int(self.input_size/2)
 
             for slice in range(raw_mri_sample.shape[2]):
                 images.append(raw_mri_sample[n11:n12, n11:n12, slice])
-        
+
         images = np.array(images)
         images = np.reshape(images, newshape=[-1, self.input_size, self.input_size, 1])
 
-        if self.load_train:
-            if self.normalization == 'standard': # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html#sklearn.preprocessing.StandardScaler
-                train_mean = np.mean(images)
-                train_sd = np.std(images)
+        if self.normalization == 'standard':  # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html#sklearn.preprocessing.StandardScaler
+            subset_mean = np.mean(images)
+            subset_sd = np.std(images)
 
-                images = (images-train_mean)/train_sd
-            else:
-                print('I dunno')
+            images = (images-subset_mean)/subset_sd
+        else:
+            print('I dunno')
+        
+        self.total_samples = np.shape(images)[0]
+
         return images
 
     '''
@@ -136,6 +155,7 @@ class CustomDataset(tf.keras.utils.Sequence):
         1. input: self
         2. output: output data
     '''
+
     def get_output(self):
         labels = []
 
@@ -144,20 +164,24 @@ class CustomDataset(tf.keras.utils.Sequence):
         else:
             number_of_patient = self.n_test_patients
 
-        for patient in number_of_patient:
+        for patient in range(number_of_patient):
 
             if self.load_train:
-                cavity_mri_sample = load_nrrd(os.path.join("../dataset/LA_dataset/Training Set", self.train_files[patient], 'lgemri.nrrd')) // 255
+                cavity_mri_sample = self.load_nrrd(os.path.join(
+                    "../dataset/LA_dataset/Training Set", self.train_files[patient], 'lgemri.nrrd')) // 255.0
             else:
-                cavity_mri_sample = load_nrrd(os.path.join("../dataset/LA_dataset/Testing Set", self.test_files[patient], 'laendo.nrrd')) // 255
+                cavity_mri_sample = self.load_nrrd(os.path.join(
+                    "../dataset/LA_dataset/Testing Set", self.test_files[patient], 'laendo.nrrd')) // 255.0
 
-            cavity_mri_sample = np.rollaxis(cavity_mri_sample, 0, 3) # (width, height, slice)
+            cavity_mri_sample = np.rollaxis(
+                cavity_mri_sample, 0, 3)  # (width, height, slice)
             midpoint = cavity_mri_sample.shape[0]//2
-            n11, n12 = midpoint - int(self.input_size/2), midpoint + int(self.input_size/2)
+            n11, n12 = midpoint - \
+                int(self.input_size/2), midpoint + int(self.input_size/2)
 
             for slice in range(cavity_mri_sample.shape[2]):
                 labels.append(cavity_mri_sample[n11:n12, n11:n12, slice])
-        
+
         labels = np.array(labels)
 
         '''
@@ -165,14 +189,16 @@ class CustomDataset(tf.keras.utils.Sequence):
         When separated the binary segmentation task into 2 channels, the first channel is for segmentation of value '0'. And the second is for
         The channel 2 are still the same.
         '''
-        temp = np.empty(shape=[labels.shape[0], self.input_size, self.input_size, 2])
-        temp[:,:,:,0] = 1 - labels
-        temp[:,:,:,1] = labels
-        label = np.reshape(temp, newshape=[-1, self.input_size, self.input_size, 1])
+        temp = np.empty(
+            shape=[labels.shape[0], self.input_size, self.input_size, 2])
+        temp[:, :, :, 0] = 1 - labels
+        temp[:, :, :, 1] = labels
+        label = np.reshape(
+            temp, newshape=[-1, self.input_size, self.input_size, 2])
 
         return label
-    
-    
+
+
 '''
 Note:
 1. The code for get input and get output are quite similar as they were stacked together in the original code.
